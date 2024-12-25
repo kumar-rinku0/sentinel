f3b1d22d9e7983bb1962a9c8872a4627fea9042f
rinku kumar <rinkukumar412616@gmail.com>
2024-12-26 02:44:59 +0530

    google oauth added

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
+    host: "127.0.0.1",
+    port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://127.0.0.1:8000",
         changeOrigin: true,
       },
     },
